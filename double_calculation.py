077ef184b8e862957e87548074c55bff9f0504eb
desigancode_journey <marinedesigan@gmail.com>
2026-02-19 17:13:13 +0530

    triple_calculation.py

--- double_calculation.py
+++ double_calculation.py
@@ -1,25 +1,30 @@
 #this is first coding to github intro
-a = 33
-b = 28
-valued_0 = input("Enter the operation you want: ")
+a = (int(input("enter the first value = ")))
+b = (int(input("enter the second value = ")))
+c = (int(input("enter the third value = ")))
+valued_0 = input("Enter the first operation you want: ")
 valued_1 = input("Enter the second operation you want: ")
-def calculator(valued, a, b):
-    if valued == "addition":
-        print(a + b)
-    elif valued == "subtraction":
-        print(a - b)
-    elif valued == "multiplication":
-        print(a * b)
-    elif valued == "division":
-        print(a / b)
-    elif valued == "floor division":
-        print(a // b)
-    elif valued == "modulus":
-        print(a % b)
+valued_2 = input("enter the third operation you want: ")
+
+def calculator(valued, a, b ,c ):
+    if valued == "addition" or "add" or "sum" :
+        print(a + b + c )
+    elif valued == "subtraction" or "sub" or "minus":
+        print(a - b - c )
+    elif valued == "multiplication" or "mul" or "product":
+        print(a * b * c )
+    elif valued == "division" or "div" or "divide":
+        print(a / b / c)
+    elif valued == "floor division" or "floordiv":
+        print(a // b //c)
+    elif valued == "modulus" or "mod":
+        print(a % b % c)
     else:
-        print("error operation")
-# new project for double calculation
+        print("this operation not available")
+# new project for triple calculation
 print("Enter first operation")
-calculator(valued_0, a, b)
+calculator(valued_0, a, b , c)
 print("Enter the second operation")
-calculator(valued_1, a, b)
+calculator(valued_1, a, b , c)
+print("enter the third operation")
+calculator(valued_2, a ,b , c)
